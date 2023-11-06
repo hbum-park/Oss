d3c82bbebceb9313793c7126b6995e04ed8fe8d9
hbum-park <heetae2632@icloud.com>
2023-11-06 10:06:51 +0900

    Delete 20231106.py

--- 20231106.py
+++
@@ -1,9 +0,0 @@
-def solution(n):
-    hap = 0
-    if(n%2==1):
-        for i in range(1,n+1,2):
-            hap+=i
-    else:
-        for i in range(2,n+1,2):
-            hap+=i**2
-    return hap
